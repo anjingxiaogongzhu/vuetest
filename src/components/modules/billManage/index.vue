7e929855d1f3657111a0630958a1ff9f9a1e130c
anjingxiaogongzhu <15267085944@163.com>
2018-01-05 11:23:12 +0800

    555

--- src/components/modules/billManage/index.vue
+++ src/components/modules/billManage/index.vue
@@ -131,6 +131,10 @@
   export default {
     data(){
       return {
+        currentPage1: 5,
+        currentPage2: 5,
+        currentPage3: 5,
+        currentPage4: 4,
         searchCell: false,
         formInline: {
           user: "",
@@ -218,6 +222,12 @@
     methods: {
       search() {
         
+      },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
       }
     }
   }
--- src/components/modules/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  login: require('./login/'),
-  mainFrame: require('./mainFrame')
-}
